a63702b9760e2ab6e916cb156912929d79734e94
YasinskiyAlexander <aleksandrasinskij@gmail.com>
2022-05-23 03:04:43 +0300

    Create component Counter

---
+++ src/components/Counter.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+export class Counter extends React.Component{
+    static defaultProps = {
+        value: 0,
+        min: 0,
+        max: 20
+    };
+    constructor(props){
+        super(props);
+        this.state = {
+            value: props.value
+        };
+    }
+    Increment(){
+        this.setState(
+            (state, props) => {
+                if(state.value < props.max)
+                    return {
+                        value: state.value +1
+                    };
+            }
+        )
+    }
+    Decrement(){
+        this.setState(
+            (state, props) => {
+                if(state.value > props.min)
+                    return {
+                        value: state.value -1
+                    };
+            }
+        )
+    }
+    render(){
+        return(
+            <div className="counter">
+                <div className="count">{this.state.value}</div>
+                <div className="buttons">
+                <button onClick={this.Increment.bind(this)}>&#8743;</button>
+                <button onClick={this.Decrement.bind(this)}>&#8744;</button>
+                </div>
+            </div>
+        );
+    }
+}
